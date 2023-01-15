8c16889251fd2e08bdfe70628b469c6d4eeb910f
Jefry16 <jefrycayo16@gmail.com>
2023-01-15 19:46:34 +0100

    added pos sidebar

---
+++ src/components/pos-sidebar.tsx
@@ -0,0 +1,50 @@
+import styles from "./pos-sidebar.module.scss";
+import Cavelogo from "../assets/cave-logo.svg";
+import ProductIcon from "../assets/products-icon.svg";
+import OpenBarIcon from "../assets/open-bar.svg";
+import VipIcon from "../assets/vip-icon.svg";
+import GroupIcon from "../assets/group-icon.svg";
+import InternetIcon from "../assets/internet-icon.svg";
+import { NavLink } from "react-router-dom";
+
+export default function PosSidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.logoCave}>
+        <img src={Cavelogo} className={styles.logoCave} />
+      </div>
+      <ul className={styles.menu}>
+        <li className={styles.linkContainer}>
+          <NavLink className={styles.link} to="/punto-de-venta/productos">
+            <img src={ProductIcon} />
+            <span>PRODUCTOS</span>
+          </NavLink>
+        </li>
+        <li className={styles.linkContainer}>
+          <NavLink className={styles.link} to="/punto-de-venta/productos">
+            <img src={OpenBarIcon} />
+            <span>OPEN BAR</span>
+          </NavLink>
+        </li>
+        <li className={styles.linkContainer}>
+          <NavLink className={styles.link} to="/punto-de-venta/productos">
+            <img src={VipIcon} />
+            <span>MESAS VIP</span>
+          </NavLink>
+        </li>
+        <li className={styles.linkContainer}>
+          <NavLink className={styles.link} to="/punto-de-venta/productos">
+            <img src={GroupIcon} />
+            <span>EVENTOS</span>
+          </NavLink>
+        </li>
+        <li className={styles.linkContainer}>
+          <NavLink className={styles.link} to="/punto-de-venta/productos">
+            <img src={InternetIcon} />
+            <span>INTERNET</span>
+          </NavLink>
+        </li>
+      </ul>
+    </aside>
+  );
+}
--- src/layouts/pos.tsx
+++ src/layouts/pos.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-import styles from "../styles/sass/admin-layout.module.scss";
+import PosSidebar from "../components/pos-sidebar";
+import styles from "../styles/sass/pos-layout.module.scss";
 export default function PosLayout() {
   return (
-    <div className={styles.admin}>
-        oooooo
+    <div className={styles.container}>
+      <PosSidebar />
       <div id="main" className={styles.main}>
         <Outlet />
       </div>
