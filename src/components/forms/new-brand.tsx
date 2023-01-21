cbbe99bfcf40064647fabd44f5441411da5f59ea
Jefry16 <jefrycayo16@gmail.com>
2023-01-21 15:53:47 +0100

    refactor brands handler

--- src/components/forms/new-brand.tsx
+++ src/components/forms/new-brand.tsx
@@ -1,9 +1,11 @@
 import { Col, Row, Form, Button, Input } from "antd";
 import { NotificationInstance } from "antd/es/notification/interface";
 import { useState } from "react";
+import useAxiosPrivate from "../../hooks/use-axios-private";
+import useHttpPost from "../../hooks/use-http-post";
+import cancelButtonStyles from "../../styles/components/cancel-button";
 import inputStyle from "../../styles/components/input";
 import labelStyle from "../../styles/components/label";
-import cancelButtonStyles from "../../styles/components/cancel-button";
 import submitButtonStyle from "../../styles/components/submit-button";
 
 const { Item } = Form;
@@ -13,9 +15,79 @@ export default function NewBrand(props: {
   setRefetch: Function;
   api: NotificationInstance;
 }) {
+  const axiosPrivate = useAxiosPrivate();
   const [brandSearch, setBrandSearch] = useState("qwertyuiop");
-  const [formData, setFormData] = useState({});
- 
+  const { sendRequest: sendPost } = useHttpPost();
 
-  return <div>ok</div>
+  return (
+    <Form
+      onFinish={async (values) => {
+        sendPost({
+          url: "brands",
+          data: { name: values.name },
+          onSuccess: () => {
+            props.onClose();
+            props.setRefetch();
+          },
+          onError: () => {
+            props.onClose();
+            props.api.error({
+              message: "Algo salió mal. Intentelo más tarde.",
+            });
+          },
+        });
+      }}
+    >
+      <Row>
+        <Col span={24}>
+          <Item
+            name="name"
+            label={<label style={labelStyle}>Nombre de la marca</label>}
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            rules={[
+              { required: true, message: "Añada un nombre" },
+              { max: 255, message: "El nombre es muy largo" },
+              {
+                validator: async () => {
+                  try {
+                    await axiosPrivate.get(
+                      `brands?name=${brandSearch}&exact=ok`
+                    );
+                    return Promise.reject(new Error("Esta marca ya existe."));
+                  } catch (error) {
+                    return Promise.resolve();
+                  }
+                },
+              },
+            ]}
+          >
+            <Input
+              autoComplete="off"
+              onChange={({ target }) => {
+                setBrandSearch(target.value);
+              }}
+              style={inputStyle}
+            />
+          </Item>
+          <div
+            style={{ display: "flex", justifyContent: "flex-end", gap: "20px" }}
+          >
+            <Button
+              style={cancelButtonStyles}
+              children="Cancelar"
+              onClick={() => props.onClose()}
+              htmlType="button"
+            />
+            <Button
+              style={submitButtonStyle}
+              type="primary"
+              children="Guardar"
+              htmlType="submit"
+            />
+          </div>
+        </Col>
+      </Row>
+    </Form>
+  );
 }
